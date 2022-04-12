58f35b6d638e7014eeae4ba2829e981ec8d1cef6
chad moore <thryce85@tamu.edu>
2022-04-12 15:40:18 -0500

    Fixed most of assignment 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from './Header.js' ;
 import Footer from './Footer.js' ;
 // https://stackoverflow.com/questions/44128959/multiple-components-within-a-file
+import  './App.css';
 import Main ,{HornedBeast} from './Main.js' ;
 
 
@@ -11,8 +12,8 @@ class App  extends React.Component{
 
     return(  <>
              <Header />
-             <HornedBeast src='https://nypost.com/wp-content/uploads/sites/2/2015/10/1_mpm-comedy_wildlife_awards05.jpg?quality=80&strip=all&w=1024'  alt="Not Available" title='A deer' description='Some stuff' />
-             <HornedBeast src='' alt="Not Available" title='Will be a picture' description='Cant get image to work so dont have a description yet.' />
+             <HornedBeast src="https://nypost.com/wp-content/uploads/sites/2/2015/10/1_mpm-comedy_wildlife_awards05.jpg?quality=80&strip=all&w=1024"s  alt="Not Available" title='A deer' description='Some stuff' />
+             <HornedBeast src='https://assetsnffrgf-a.akamaihd.net/assets/m/502014128/univ/art/502014128_univ_lsr_xl.jpg' alt="Not Available" title='Will be a picture' description='' />
             <Footer  />
             </>   ) ;
 
