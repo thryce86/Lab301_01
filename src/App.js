15bfd03ccd8fa8c1a8f8a5ae67dfec74c5e7ae03
chad moore <thryce85@tamu.edu>
2022-04-12 15:43:31 -0500

    removed main from import on app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './Header.js' ;
 import Footer from './Footer.js' ;
 // https://stackoverflow.com/questions/44128959/multiple-components-within-a-file
 import  './App.css';
-import Main ,{HornedBeast} from './Main.js' ;
+import {HornedBeast} from './Main.js' ;
 
 
 class App  extends React.Component{
